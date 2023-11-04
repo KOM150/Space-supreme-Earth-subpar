a8b890729a3ef21899d9fa24e1345f8ae1ad1a1d
KOM150 <112767611+KOM150@users.noreply.github.com>
2023-11-04 20:07:58 +0900

    1. 게임 매니저 구현 2. 슬라이더에 미니 게임 점수 반영
    싱글톤 패턴을 게임 매니저에 상속하여, 게임 매니저가 미니 게임에도 적용되도록 구현하였다. 그리고 게임 매니저의 점수를 미니 게임인 "shooting space ship"씬의 점수로 활용하고, 이를 "Tycoon Scene"씬의 Slider에 반영하였다.

--- Assets/Scripts/Shooting Space Ship/GameController.cs
+++ Assets/Scripts/Shooting Space Ship/GameController.cs
@@ -5,97 +5,83 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject[] hazards; //소환 오브젝트 배열 
-    public Vector3 spawnValues; //소환 오브젝트 영역 제한 (x, y, z)
-    public int hazardCount; //소환 오브젝트 배열 크기
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount; 
 
-    //게임 난이도 조절
-    public float startWait; //게임이 시작되고 얼마나 빨리 소환 오브젝트가 나타날지
-    public float spawnWait; //소환 오브젝트 생성 간격
-    public float waveWait; //소환 오브젝트 생성이 시작되기 전의 간격
+    public float startWait; 
+    public float spawnWait; 
+    public float waveWait; 
 
-    public Text titleText; //게임 실행 시 Score Text로 사용
+    public Text titleText; 
     public Text memoText;
     public Text subtitleText;
 
-    public bool gameOver; //게임 종료 여부
-    private bool restart; //재실행 여부
-    private int score;
-    public int gameOverScore;
+    public bool gameOver;
+    private bool restart;
+
+    //
+    private GameManager gameManager;
 
     void Start()
     {
+        gameManager = GameManager.Instance;
         StartCoroutine(WaitAndStart());
     }
 
     void UpdateScore()
     {
-        // Enemy 20점, 행성 10점 이러한 점수 값을 어떻게 해당 코드에 반영했는지
-        // > 바로 AddScore()를 사용
-        // > Done_Destory by Contact 스크립트에서 Done_GameController 객체를 받아와, AddScore()를 사용
-        titleText.text = "Score: " + score;
+        titleText.text = "Score: " + gameManager._score;
     }
 
     public void AddScore(int newScoreValue)
     {
-        score += newScoreValue;
+        gameManager._score += newScoreValue;
         UpdateScore();
     }
 
     IEnumerator WaitAndStart()
     {
-        yield return new WaitForSeconds(0.5f); //0.5초 대기 > 게임 실행
+        yield return new WaitForSeconds(0.5f);
 
         gameOver = false;
         restart = false;
 
-        memoText.text = ""; //게임 실행하면 화면에서 사라짐
+        memoText.text = "";
         subtitleText.text = "";
 
-        //점수 시스템
-        score = 0;
+        gameManager._score = 0;
         UpdateScore();
 
-        //소환 및 재실행 시스템
-        //코루틴
-        /*
-          1. 게임 루프의 차단 방지: 코루틴을 사용하면 긴 작업을 수행해도 게임 루프를 차단하지 않고 계속 실행 > 부드러운 게임 실행 유지
-          2. 시간 지연 및 간격 제어: yield return new WaitForSeconds(time)
-          3. 병렬 작업 처리
-         */
         StartCoroutine(SpawnWaves());
     }
 
     IEnumerator SpawnWaves()
     {
-        yield return new WaitForSeconds(startWait); //1. 게임이 시작되고 얼마나 빨리 소환 오브젝트가 나타날지
+        yield return new WaitForSeconds(startWait); 
 
-        //무한
         while (true)
         {
-            //소환 시스템 //3. 소환 오브젝트 생성 간격
             for (int i = 0; i < hazardCount; i++)
             {
-                GameObject hazard = hazards[Random.Range(0, hazards.Length)]; //행성 1~3과 적 중 랜덤 생성
-                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z); //소환 오브젝트 위치
-                Quaternion spawnRotation = Quaternion.identity; //소환 오브젝트 각도 = 회전을 갖지 않는 초기 상태
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)]; 
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z); 
+                Quaternion spawnRotation = Quaternion.identity; 
 
-                //Instantiate(): 게임을 도중에 게임 오브젝트를 생성
                 Instantiate(hazard, spawnPosition, spawnRotation);
 
-                yield return new WaitForSeconds(spawnWait); //다음 i(index)로 가기 위해 얼마나 기다릴 것이냐
+                yield return new WaitForSeconds(spawnWait);
             }
 
-            yield return new WaitForSeconds(waveWait); //2. 소환 오브젝트 생성이 시작되기 전의 간격 // Wave 간격 // 그룹 간격
+            yield return new WaitForSeconds(waveWait); 
 
-            //재실행 시스템
             if (gameOver)
             {
                 memoText.text = "Press 'R' for GameOver";
                 memoText.fontSize = 30;
                 memoText.color = Color.white;
                 restart = true;
-                break; //While문 탈출
+                break;
             }
         }
     }
@@ -107,8 +93,6 @@ public class GameController : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.R))
             {
-                //SceneManager.LoadScene() : 현재 활성화된 씬을 다시 로드함으로써 씬을 다시 시작하거나 초기 상태
-                //SceneManager.GetActiveScene().buildIndex : 현재 활성화된 씬의 빌드 인덱스
                 SceneManager.LoadScene("Tycoon Scene");
             }
         }
@@ -121,6 +105,5 @@ public class GameController : MonoBehaviour
         subtitleText.fontStyle = FontStyle.Bold;
         subtitleText.color = Color.yellow;
         gameOver = true;
-        gameOverScore = score;
     }
 }
---
+++ Assets/Scripts/Tycoon Scene/Singleton.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+
+                if (_instance == null)
+                {
+                    GameObject game_object = new GameObject();
+                    game_object.name = typeof(T).Name;
+                    _instance = game_object.AddComponent<T>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
